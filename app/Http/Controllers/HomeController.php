e70071be0d19791214e2c28139fbed0732e7e3d2
ahmad m <karkas.games1212@gmail.com>
2020-01-05 14:50:42 +0330

    add route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,10 +11,10 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     /**
      * Show the application dashboard.
@@ -23,6 +23,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        echo("hello");
+        // return view('home');
+    }
+    public function hi()
+    {
+        echo("hello");
+        // return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('home' , 'homeController@index');
+Route::get('hi' , 'homeController@hi');
