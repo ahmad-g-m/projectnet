7b2c90d1e64b6a0ba55af779799728e33e0c5dff
mehdikhorsand <mehdikhorsan98@gmail.com>
2020-01-19 19:39:17 +0330

    update sign in page

--- resources/views/user/formLoginUser.blade.php
+++ resources/views/user/formLoginUser.blade.php
@@ -61,13 +61,3 @@
 	<script src="bootstrap.min.js"></script>
 </body>
 </html>
-
-
-
-<form action="logineduser" method="post">
-    {!!csrf_field()!!}
-    email : <input type="text" name="email">
-    password : <input type="text" name="password">
-    <br>
-    <input type="submit" name="btn" value="login">
-</form>
