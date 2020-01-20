7d05e4776feea7d69b6f9c65a5b55e4ab00916c9
ahmad <karkas.games1212@gmail.com>
2020-01-20 23:15:54 +0330

    nothig

---
+++ resources/views/index1.blade.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <a href="http://127.0.0.1:8000/loginadmin" >login admin</a><br>
+    <a href="http://127.0.0.1:8000/loginuser" >login user</a><br>
+    <a href="http://127.0.0.1:8000/registeradmin" >register admin</a><br>
+    <a href="http://127.0.0.1:8000/registeruser" >register user</a><br><br>
+    
+    <a href="http://127.0.0.1:8000/machines" >all machines</a><br>
+    <a href="http://127.0.0.1:8000/machines/sedanscars" >cars</a><br>
+    <a href="http://127.0.0.1:8000/machines/classicscars" >classic cars</a><br>
+    <a href="http://127.0.0.1:8000/machines/sportscars" >sports cars</a><br><br>
+    <p> salam khodafez ha ha ha ha za za</p>
+    <form action="http://127.0.0.1:8000/search" method="get">
+        <input type="search" name="searchtxt" id="searchtxt">
+        <input type="submit" value="search" name="searchbtn" id="searchbtn">
+    </form>
+</body>
+</html>
