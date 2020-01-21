cc2a0199ab4e84f8a21b2e535b96f0ff5946075d
ahmad <karkas.games1212@gmail.com>
2020-01-21 03:56:36 +0330

    compeleted category

--- app/Http/Controllers/carsHomeController.php
+++ app/Http/Controllers/carsHomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Cars;
+use App\Address;
+use Illuminate\Support\Facades\Storage;
 
 class carsHomeController extends Controller
 {
@@ -14,7 +16,26 @@ class carsHomeController extends Controller
         //     echo($car->name);
         //     echo("<br>");
         // }
-        return view('cars.showclassCars' , ['moclass' => 9 , 'all_cars' => $all_cars]);
+        $slideshowcar = Cars::orderBy('id','desc')->take(10)->get();
+        $address_arr = array();
+        $car_id = array();
+        $count = 0;
+        foreach($slideshowcar as $sl){
+            if(Address::where('mark',$sl->id)){
+            $addr = Address::where('mark',$sl->id)->first();
+            array_push($address_arr,$addr->address);
+            array_push($car_id,$sl->id);
+            $count++;
+            }
+            if($count == 3){
+                break;
+            }
+        }
+        $find3cars = Cars::find([$car_id[0],$car_id[1],$car_id[2]]);
+        $contents = array();
+        array_push($contents,Storage::get($address_arr[0]),Storage::get($address_arr[1]),Storage::get($address_arr[2]));
+
+        return view('cars.category' , ['moclass' => 9 , 'all_cars' => $all_cars , 'slidecars'=>$find3cars , 'addresscars'=>$contents]);
     }
     public function indexSedanCars()
     {
@@ -23,7 +44,26 @@ class carsHomeController extends Controller
         //     echo($car->name);
         //     echo("<br>");
         // }
-        return view('cars.showclassCars' , ['moclass' => 1 , 'all_cars' => $all_cars]);
+        $slideshowcar = Cars::orderBy('id','desc')->take(10)->get();
+        $address_arr = array();
+        $car_id = array();
+        $count = 0;
+        foreach($slideshowcar as $sl){
+            if(Address::where('mark',$sl->id)){
+            $addr = Address::where('mark',$sl->id)->first();
+            array_push($address_arr,$addr->address);
+            array_push($car_id,$sl->id);
+            $count++;
+            }
+            if($count == 3){
+                break;
+            }
+        }
+        $find3cars = Cars::find([$car_id[0],$car_id[1],$car_id[2]]);//for slide show
+        $contents = array();
+        array_push($contents,Storage::get($address_arr[0]),Storage::get($address_arr[1]),Storage::get($address_arr[2]));
+
+        return view('cars.category' , ['moclass' => 1 , 'all_cars' => $all_cars , 'slidecars'=>$find3cars , 'addresscars'=>$contents]);
     }
     public function showPageCars(Request $r , $id){
         if(Cars::find($id)){
@@ -40,7 +80,26 @@ class carsHomeController extends Controller
         //     echo($car->name);
         //     echo("<br>");
         // }
-        return view('cars.showclassCars' , ['moclass' => 2 , 'all_cars' => $all_cars]);
+        $slideshowcar = Cars::orderBy('id','desc')->take(10)->get();
+        $address_arr = array();
+        $car_id = array();
+        $count = 0;
+        foreach($slideshowcar as $sl){
+            if(Address::where('mark',$sl->id)){
+            $addr = Address::where('mark',$sl->id)->first();
+            array_push($address_arr,$addr->address);
+            array_push($car_id,$sl->id);
+            $count++;
+            }
+            if($count == 3){
+                break;
+            }
+        }
+        $find3cars = Cars::find([$car_id[0],$car_id[1],$car_id[2]]);
+        $contents = array();
+        array_push($contents,Storage::get($address_arr[0]),Storage::get($address_arr[1]),Storage::get($address_arr[2]));
+
+        return view('cars.category' , ['moclass' => 2 , 'all_cars' => $all_cars , 'slidecars'=>$find3cars , 'addresscars'=>$contents]);
     }
     public function indexSportsCars()
     {
@@ -50,6 +109,25 @@ class carsHomeController extends Controller
         //     echo($car->name);
         //     echo("<br>");
         // }
-        return view('cars.showclassCars' , ['moclass' => 3 , 'all_cars' => $all_cars]);
+        $slideshowcar = Cars::orderBy('id','desc')->take(10)->get();
+        $address_arr = array();
+        $car_id = array();
+        $count = 0;
+        foreach($slideshowcar as $sl){
+            if(Address::where('mark',$sl->id)){
+            $addr = Address::where('mark',$sl->id)->first();
+            array_push($address_arr,$addr->address);
+            array_push($car_id,$sl->id);
+            $count++;
+            }
+            if($count == 3){
+                break;
+            }
+        }
+        $find3cars = Cars::find([$car_id[0],$car_id[1],$car_id[2]]);
+        $contents = array();
+        array_push($contents,Storage::get($address_arr[0]),Storage::get($address_arr[1]),Storage::get($address_arr[2]));
+
+        return view('cars.category' , ['moclass' => 3 , 'all_cars' => $all_cars ,  'slidecars'=>$find3cars , 'addresscars'=>$contents]);
     }
 }
